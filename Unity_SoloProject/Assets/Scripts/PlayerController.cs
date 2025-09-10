afb731e209bd468548e29ecbf5419670b339e1a5
ssintlurks <katelynn.n.mortensen@gmail.com>
2025-09-10 13:22:25 -0700

    PlayerMovement 9/10

---
+++ Unity_SoloProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    float inputX;
+    float inputY;
+
+    public float speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempMove = rb.linearVelocity;
+
+        tempMove.x = inputY * speed;
+        tempMove.z = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.forward) +
+            (tempMove.y * transform.up) + 
+            (tempMove.z * transform.right);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+
+        inputX = InputAxis.x;
+        inputY = InputAxis.y;
+
+    }
+
+
+
+}
+ 
