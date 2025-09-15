b24c7a7fef3d6a6c14a46865e0e3db4f36b16343
ssintlurks <katelynn.n.mortensen@gmail.com>
2025-09-15 12:58:54 -0700

    Cinemachine 9/15

--- Unity_SoloProject/Assets/Scripts/PlayerController.cs
+++ Unity_SoloProject/Assets/Scripts/PlayerController.cs
@@ -3,26 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector3 cameraOffset = new Vector3(0,.5f,.2f);
-    Vector2 cameraRotation = Vector2.zero;
     Camera playerCam;
-    InputAction lookAxis;
     private Rigidbody rb;
 
     float inputX;
     float inputY;
 
-    public float Xsensitivity = .1f;
-    public float Ysensitivity = .1f;
     public float speed = 5f;
-    public float camRotationLimit = 90;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerCam = Camera.main;
-        lookAxis = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -32,13 +25,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Camera Handler
-        playerCam.transform.position = transform.position + cameraOffset;
-
-        cameraRotation.x += lookAxis.ReadValue<Vector2>().x * Xsensitivity;
-        cameraRotation.y += lookAxis.ReadValue<Vector2>().y * Ysensitivity;
-        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);
-        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
-        transform.rotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        Quaternion playerRotation = playerCam.transform.rotation;
+        playerRotation.x = 0;
+        playerRotation.z = 0;
+        transform.rotation = playerRotation;
 
         // Movement System
         Vector3 tempMove = rb.linearVelocity;
