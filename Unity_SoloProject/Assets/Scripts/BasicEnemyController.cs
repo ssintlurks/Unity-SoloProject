0e03178752aba40a607eb63e95af1d1098c4d290
ssintlurks <katelynn.n.mortensen@gmail.com>
2025-09-25 13:37:39 -0700

    UI Menus 9/25

--- Unity_SoloProject/Assets/Scripts/BasicEnemyController.cs
+++ Unity_SoloProject/Assets/Scripts/BasicEnemyController.cs
@@ -3,19 +3,43 @@ using UnityEngine.AI;
 
 public class BasicEnemyController : MonoBehaviour
 {
+    PlayerController player;
+
     NavMeshAgent agent;
+    public bool isFollowing = false;
+
+    public int health = 5;
+    public int maxHealth = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agent= GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            if (isFollowing)
+                agent.destination = player.transform.position;
+        }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "proj")
+        {
+            health--;
+            Destroy(collision.gameObject);
+        }
+    }
 
 }
